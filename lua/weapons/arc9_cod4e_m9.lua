a66f3f87847bdea7c5fc684e87abdf9ed9f1cd9c
PalindroneV2 <asfadasfag@gmail.com>
2022-04-06 19:28:43 -0700

    m0

--- lua/weapons/arc9_cod4e_m9.lua
+++ lua/weapons/arc9_cod4e_m9.lua
@@ -358,7 +358,7 @@ SWEP.Animations = {
     },
     ["enter_sprint"] = {
         Source = "sprint_in",
-        Time = 10 / 30
+        Time = 1,
     },
     ["idle_sprint"] = {
         Source = "sprint_loop",
@@ -366,11 +366,11 @@ SWEP.Animations = {
     },
     ["exit_sprint"] = {
         Source = "sprint_out",
-        Time = 10 / 30
+        Time = 1,
     },
     ["enter_sprint_empty"] = {
         Source = "sprint_in_empty",
-        Time = 10 / 30
+        Time = 1,
     },
     ["idle_sprint_empty"] = {
         Source = "sprint_loop_empty",
@@ -378,7 +378,7 @@ SWEP.Animations = {
     },
     ["exit_sprint_empty"] = {
         Source = "sprint_out_empty",
-        Time = 10 / 30
+        Time = 1,
     },
 
     -- ELITE RELOAD --
