6eba7947a3c871d09a1d8764ca8af9f5f6c7b36f
PalindroneV2 <asfadasfag@gmail.com>
2022-04-14 23:31:58 -0700

    pistol rails are fixed

--- lua/arc9/common/attachments/mwc_tac_pistol_rail.lua
+++ lua/arc9/common/attachments/mwc_tac_pistol_rail.lua
@@ -11,7 +11,7 @@ ATT.ActivateElements = {"pistol_rail"}
 
 ATT.Model = "models/weapons/arc9/atts/mw2_pistol_rail.mdl"
 ATT.Scale = 0.375
-ATT.ModelOffset = Vector(-1.5, -0.2,-0.25)
+ATT.ModelOffset = Vector(-1.5, 0.01,-0.25)
 ATT.Bonemerge = false
 
 ATT.Attachments = {
@@ -19,7 +19,7 @@ ATT.Attachments = {
         PrintName = "Optic",
         Bone = "j_gun",
         Scale = Vector(1,1,1),
-        Pos = Vector(4, -0.25, -2.125),
+        Pos = Vector(4, 0, -2.125),
         Ang = Angle(0, 0, 0),
         Icon_Offset = Vector(0, 0, 1.5),
         -- CorrectiveAng = Angle(-1, -1, 0),
@@ -30,7 +30,7 @@ ATT.Attachments = {
         PrintName = "Tactical",
         DefaultCompactName = "TAC",
         Bone = "j_gun",
-        Pos = Vector(-1, -0.25, 0.2),
+        Pos = Vector(-1, 0, 0.2),
         Ang = Angle(0, 0, 0),
         Category = {"bo1_tactical"},
     },
