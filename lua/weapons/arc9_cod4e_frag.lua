53c4549bad4a385e3cceaddc25c07957bb7f97b5
PalindroneV2 <asfadasfag@gmail.com>
2022-11-08 00:22:55 -0700

    placeholder grenade lua

---
+++ lua/weapons/arc9_cod4e_frag.lua
@@ -0,0 +1,296 @@
+SWEP.Base = "arc9_base"
+SWEP.Spawnable = true -- this obviously has to be set to true
+SWEP.Category = "ARC9 - COD4: Modern Warfare" -- edit this if you like
+SWEP.AdminOnly = false
+
+SWEP.PrintName = "M67 Frag Grenade"
+SWEP.Class = "Grenade"
+SWEP.Description = [[
+    Fragmentation hand grenade used by the United States military.
+]]
+SWEP.Trivia = {
+    Country = "USA",
+    Year = 1967,
+    Games = [[Many CODs]]
+}
+SWEP.Credits = {
+    Author = "Palindrone"
+}
+
+SWEP.Slot = 4
+
+SWEP.UseHands = true
+
+SWEP.ViewModel = "models/weapons/arc9/c_cod4_frag.mdl"
+SWEP.WorldModel = "models/weapons/arc9/c_cod4_frag.mdl"
+SWEP.MirrorVMWM = true
+SWEP.WorldModelOffset = {
+    Pos        =    Vector(-3, 5, -7.5),
+    Ang        =    Angle(5, 7, 180),
+    Bone    =    "ValveBiped.Bip01_R_Hand",
+    Scale = 1.1,
+}
+SWEP.ViewModelFOVBase = 75
+
+SWEP.DefaultBodygroups = "00000000000000"
+
+SWEP.DamageMax = 25
+SWEP.DamageMin = 15 -- damage done at maximum range
+SWEP.RangeMax = 6000
+SWEP.RangeMin = 1000
+SWEP.Penetration = 0
+SWEP.DamageType = nil
+SWEP.ShootEnt = "arc9_mwc_m203_he" -- Set to an entity to launch it out of this weapon.
+SWEP.ShootEntForce = 12500
+SWEP.ShootEntityData = {} -- Extra data that can be given to a projectile. Sets SENT.WeaponDataLink with this table.
+
+SWEP.PhysBulletMuzzleVelocity = 960 * 39.37
+
+SWEP.BodyDamageMults = {
+    [HITGROUP_HEAD] = 2,
+    [HITGROUP_CHEST] = 1,
+    [HITGROUP_LEFTARM] = 1,
+    [HITGROUP_RIGHTARM] = 1,
+    [HITGROUP_LEFTLEG] = 1,
+    [HITGROUP_RIGHTLEG] = 1,
+}
+
+SWEP.ChamberSize = 0 -- dont fucking change this again.
+SWEP.ClipSize = 1 -- DefaultClip is automatically set.
+SWEP.ReloadTime = 1
+
+SWEP.Crosshair = true
+SWEP.LauncherCrosshair = true
+SWEP.CanBlindFire = false
+
+SWEP.Recoil = 2
+SWEP.RecoilSide = 0.5
+SWEP.RecoilUp = 2
+
+SWEP.RecoilRandomUp = 0.5
+SWEP.RecoilRandomSide = 0.25
+
+SWEP.RecoilDissipationRate = 40 -- How much recoil dissipates per second.
+SWEP.RecoilResetTime = 0.1 -- How long the gun must go before the recoil pattern starts to reset.
+
+SWEP.RecoilAutoControl = 0.5
+SWEP.RecoilKick = 0
+SWEP.PushBackForce = 1
+
+SWEP.Spread = 0
+SWEP.SpreadAddRecoil = 0
+
+SWEP.SpreadMultSights = 0.1
+SWEP.SpreadAddHipFire = 0
+--SWEP.SpreadAddMove = 0
+SWEP.SpreadAddMidAir = 0
+-- SWEP.SpreadAddShooting = math.rad(5 / 37.5) -- 0 -- = 0
+
+SWEP.RecoilPatternDrift = 20
+
+SWEP.VisualRecoilUp = 1
+SWEP.VisualRecoilSide = 0
+SWEP.VisualRecoilRoll = 0
+SWEP.VisualRecoilCenter = Vector(0, 0, 0)
+SWEP.VisualRecoilPunch = 0
+SWEP.VisualRecoilMultSights = 0
+
+SWEP.Speed = 1
+
+SWEP.ShootWhileSprint = true
+SWEP.ReloadInSights = false
+
+SWEP.SpeedMultSights = 0.8
+SWEP.SpeedMultShooting = 0.75
+SWEP.SpeedMultMelee = 1
+SWEP.SpeedMultCrouch = 1
+SWEP.SpeedMultBlindFire = 1
+
+SWEP.AimDownSightsTime = 0.25
+SWEP.SprintToFireTime = 0.25
+
+SWEP.RPM = 150
+SWEP.AmmoPerShot = 1 -- number of shots per trigger pull.
+SWEP.Firemodes = {
+    {
+        Mode = -1,
+        PrintName = "SINGLE"
+    },
+}
+SWEP.NPCWeaponType = {"weapon_shotgun"}
+SWEP.NPCWeight = 10
+
+SWEP.FreeAimRadius = 0 -- In degrees, how much this gun can free aim in hip fire.
+SWEP.Sway = 0 -- How much the gun sways.
+
+SWEP.FreeAimRadiusMultSights = 0
+
+SWEP.SwayMultSights = 0
+
+SWEP.Ammo = "SMG1_Grenade" -- what ammo type the gun uses
+
+SWEP.ShootVolume = 125
+SWEP.ShootPitch = 100
+SWEP.ShootPitchVariation = 0
+
+SWEP.ShootSound = "ARC9_MW2E.M79_Fire"
+SWEP.ShootSoundSilenced = "ARC9_BO1.MP5_Sil"
+
+SWEP.MuzzleParticle = "muzzleflash_m79" -- Used for some muzzle effects.
+
+SWEP.ShellModel = "models/shells/shell_12gauge.mdl"
+SWEP.ShellScale = 1.5
+SWEP.ShellMaterial = "models/weapons/arcticcw/shell_556_steel"
+
+SWEP.MuzzleEffectQCA = 1 -- which attachment to put the muzzle on
+SWEP.CaseEffectQCA = 2 -- which attachment to put the case effect on
+SWEP.ProceduralViewQCA = 1
+SWEP.CamQCA = 3
+
+SWEP.BulletBones = {
+}
+
+SWEP.ProceduralRegularFire = false
+SWEP.ProceduralIronFire = false
+
+SWEP.CaseBones = {}
+
+SWEP.IronSights = {
+    Pos = Vector(-2.875, 0, -5.6),
+    Ang = Angle(0, 18.25, 0),
+    Magnification = 1.25,
+    CrosshairInSights = false,
+    SwitchToSound = "", -- sound that plays when switching to this sight
+}
+
+SWEP.NoShellEject = true
+
+SWEP.HoldtypeHolstered = "passive"
+SWEP.HoldtypeActive = "crossbow"
+SWEP.HoldtypeSights = "crossbow"
+
+SWEP.AnimShoot = ACT_HL2MP_GESTURE_RANGE_ATTACK_SHOTGUN
+SWEP.AnimReload = ACT_HL2MP_GESTURE_RELOAD_CROSSBOW
+SWEP.AnimDraw = ACT_HL2MP_GESTURE_RANGE_ATTACK_KNIFE
+
+SWEP.ActivePos = Vector(0, 0, -1)
+SWEP.ActiveAng = Angle(0, 0, -5)
+
+SWEP.MovingPos = SWEP.ActivePos
+SWEP.MovingAng = SWEP.ActiveAng
+
+SWEP.MovingMidPoint = {
+    Pos = SWEP.ActivePos,
+    Ang = SWEP.ActiveAng
+}
+
+SWEP.CrouchPos = Vector(0, 0, -1)
+SWEP.CrouchAng = Angle(0, 0, -5)
+
+SWEP.SprintVerticalOffset = false
+SWEP.SprintPos = Vector(0, 0, -1)
+SWEP.SprintAng = Angle(0, 0, -5)
+
+SWEP.CustomizePos = Vector(12.5, 40, 4)
+SWEP.CustomizeAng = Angle(90, 0, 0)
+
+SWEP.RestPos = Vector(0, 0, 0)
+SWEP.RestAng = Angle(0, 0, 0)
+
+SWEP.BarrelLength = 25
+
+SWEP.ExtraSightDist = 5
+
+SWEP.AttachmentElements = {
+}
+
+SWEP.Hook_ModifyBodygroups = function(self, data)
+    local vm = data.model
+    local attached = data.elements
+
+    if attached["bo1_pap"] then
+        vm:SetSkin(2)
+    end
+end
+
+SWEP.Attachments = {
+    {
+        PrintName = "Ammunition",
+        DefaultCompactName = "AMMO",
+        Bone = "j_gun",
+        Pos = Vector(3, 0, 2),
+        Ang = Angle(0, 0, 0),
+        Category = {"bo1_pap_launcher"},
+    },
+    {
+        PrintName = "Perk",
+        DefaultCompactName = "PERK",
+        Bone = "j_gun",
+        Pos = Vector(-5, 0, -5),
+        Ang = Angle(0, 0, 0),
+        Category = "mwc_perk",
+    },
+    {
+        PrintName = "Proficiency",
+        DefaultCompactName = "PRO",
+        Bone = "j_gun",
+        Pos = Vector(-8, 0, -5),
+        Ang = Angle(0, 0, 0),
+        Category = "mwc_proficiency",
+    },
+}
+
+SWEP.Animations = {
+    ["idle"] = {
+        Source = "idle",
+        Time = 1 / 35,
+    },
+    ["draw"] = {
+        Source = "draw",
+        Time = 25 / 35,
+        EventTable = {
+            {s = "ARC9_MW2E.M79_Draw", t = 1 / 35},
+        }
+    },
+    ["holster"] = {
+        Source = "holster",
+        Time = 25 / 35,
+        EventTable = {
+            {s = "ARC9_MW2E.M79_Holster", t = 1 / 35},
+        }
+    },
+    ["fire"] = {
+        Source = {
+            "fire",
+        },
+        Time = 15 / 35,
+    },
+    ["fire_iron"] = {
+        Source = {
+            "fire_ads",
+        },
+        Time = 15 / 35,
+    },
+    ["reload"] = {
+        Source = "reload",
+        Time = 93 / 35,
+        EventTable = {
+            {s = "ARC9_MW2E.M79_Open", t = 5 / 35},
+            {s = "ARC9_MW2E.M79_Out", t = 29 / 35},
+            {s = "ARC9_MW2E.M79_In", t = 61 / 35},
+            {s = "ARC9_MW2E.M79_Close", t = 83 / 35},
+        },
+    },
+    ["enter_sprint"] = {
+        Source = "sprint_in",
+        Time = 1,
+    },
+    ["idle_sprint"] = {
+        Source = "sprint_loop",
+        Time = 30 / 30
+    },
+    ["exit_sprint"] = {
+        Source = "sprint_out",
+        Time = 1,
+    },
+}
