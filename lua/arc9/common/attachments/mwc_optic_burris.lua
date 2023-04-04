265ea66ebdcac0ac43d6d58c0e5366d86e1842e7
PalindroneV2 <asfadasfag@gmail.com>
2023-04-04 02:02:53 -0700

    a

--- lua/arc9/common/attachments/mwc_optic_burris.lua
+++ lua/arc9/common/attachments/mwc_optic_burris.lua
@@ -1,71 +1,35 @@
-ATT.PrintName = "Thermal Scope"
-ATT.CompactName = [[Thermal]]
-ATT.Icon = Material("entities/mwc_atts/optics/mw2_thermal.png", "mips smooth")
-ATT.Description = [[MW2 Thermal Sight.
-Magnified optical sight that highlights enemies in yellow in a green background.]]
+ATT.PrintName = "Burris Fast Fire"
+ATT.CompactName = [[RDS MW2]]
+ATT.Icon = Material("entities/mwc_atts/optics/mw2_rds.png", "mips smooth")
+ATT.Description = [[Small, low profile optic mainly used by pistols. Provides a small electronic dot reticle which speeds up target acquisition by eliminating the need to line up irons.
+MW2 Red Dot Sight.]]
 ATT.Pros = {
-    "+ 2x Zoom",
     "+ Clearer sight picture",
-    "+ Marks targets"
 }
 ATT.Cons = {}
-ATT.SortOrder = 4
+ATT.SortOrder = 1
 ATT.MenuCategory = "ARC9 - MWC Attachments"
 ATT.Free = false
-ATT.Folder = "SCOPE/MWC"
+ATT.Folder = "RDS"
 
-ATT.Category = {"cod_optic", "cod_optic_alt"}
+ATT.Category = {"cod_optic", "cod_optic_alt", "cod_optic_lp"}
 
-
-ATT.Model = "models/weapons/arc9/atts/mw2e_thermalscope.mdl"
+ATT.Model = "models/weapons/arc9/atts/mw2e_rds_burris.mdl"
+ATT.WorldModel = "models/weapons/arc9/atts/mw2e_rds_burris_w.mdl"
 ATT.Scale = 1
-ATT.ModelOffset = Vector(-0.5, 0, 0)
+ATT.ModelOffset = Vector(0, 0, -0.05)
 
 ATT.Sights = {
     {
-        Pos = Vector(0, 7.5, -0.915),
+        Pos = Vector(0, 9, -1.05),
         Ang = Angle(0, 0, 0),
-        ViewModelFOV = 40,
-        Magnification = 1.25,
-        IgnoreExtra = true
+        ViewModelFOV = 60,
+        Magnification = 1,
+        IgnoreExtra = false
     },
 }
 
-ATT.RTScope = true
-ATT.RTScopeSubmatIndex = 0
-ATT.RTScopeFOV = 10
-ATT.RTScopeRes = 1024
-ATT.RTScopeReticle = Material("hud/arc9_mwc/scopes/mwc_thermal.png", "mips smooth")
-ATT.RTScopeShadowIntensity = 1.5
-ATT.RTScopeReticleScale = 1.375
-ATT.RTScopeNoPP = false
-ATT.RTScopeNoShadow = false
-
-ATT.RTScopeFLIR = true
-ATT.RTScopeFLIRSolid = true
-ATT.RTScopeFLIRHighlightColor = Color(200, 255, 150)
-ATT.RTScopeFLIRMonochrome = true
-ATT.RTScopeFLIRNoPP = false
-ATT.RTScopeFLIRBlend = 0.1
-ATT.RTScopeFLIRCCHot = { -- Color correction drawn only on FLIR targets
-    ["$pp_colour_addr"] = 0.5,
-    ["$pp_colour_addg"] = 0.5,
-    ["$pp_colour_addb"] = 0,
-    ["$pp_colour_brightness"] = 0,
-    ["$pp_colour_contrast"] = 0.5,
-    ["$pp_colour_colour"] = 1,
-    ["$pp_colour_mulr"] = 0,
-    ["$pp_colour_mulg"] = 0,
-    ["$pp_colour_mulb"] = 0
-}
-ATT.RTScopeFLIRCCCold = { -- Color correction drawn only on FLIR targets
-    ["$pp_colour_addr"] = 0,
-    ["$pp_colour_addg"] = 0.25,
-    ["$pp_colour_addb"] = 0,
-    ["$pp_colour_brightness"] = -0.1,
-    ["$pp_colour_contrast"] = 0.5,
-    ["$pp_colour_colour"] = 1,
-    ["$pp_colour_mulr"] = 0,
-    ["$pp_colour_mulg"] = 0,
-    ["$pp_colour_mulb"] = 0
-}
+ATT.HoloSight = true
+ATT.HoloSightReticle = Material("hud/arc9_mwc/reticles/mwc_cross.png", "mips smooth")
+ATT.HoloSightSize = 375
+ATT.HoloSightColorable = true
