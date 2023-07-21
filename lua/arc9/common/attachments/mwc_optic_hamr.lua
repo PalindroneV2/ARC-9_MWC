b967a64a017e22292a32e74cb9b2279efff220f2
PalindroneV2 <asfadasfag@gmail.com>
2023-07-21 04:26:29 -0700

    q

--- lua/arc9/common/attachments/mwc_optic_hamr.lua
+++ lua/arc9/common/attachments/mwc_optic_hamr.lua
@@ -1,8 +1,8 @@
-ATT.PrintName = "Trijicon ACOG ECOS (4x)"
-ATT.CompactName = [[ACOG MW2]]
-ATT.Icon = Material("entities/mwc_atts/optics/mw2_acog.png", "mips smooth")
+ATT.PrintName = "Leupold HAMR (4x)"
+ATT.CompactName = [[HAMR MW3]]
+ATT.Icon = Material("entities/mwc_atts/optics/mw3_hamr.png", "mips smooth")
 ATT.Description = [[Medium range combat scope for improved precision at longer ranges.
-Belongs to Modern Warfare 2.]]
+Belongs to Modern Warfare 3.]]
 ATT.Pros = {
     "+ 4x Zoom",
     "+ Clearer sight picture"
@@ -16,35 +16,20 @@ ATT.Folder = "SCOPE/MWC"
 ATT.Category = {"cod_optic", "cod_optic_alt"}
 
 
-ATT.Model = "models/weapons/arc9/atts/mw2e_acog.mdl"
+ATT.Model = "models/weapons/arc9/atts/mw3e_hamr.mdl"
 ATT.Scale = 1
-ATT.ModelOffset = Vector(0, 0,-0.15)
-ATT.DrawFunc = function(swep, model, wm)
-    if swep:GetElements()["universal_camo"] then
-        model:SetSkin(1)
-    else
-        model:SetSkin(0)
-    end
-end
+ATT.ModelOffset = Vector(0, 0,0)
 
 ATT.Sights = {
     {
-        Pos = Vector(0.0025, 6.5, -1.2),
+        Pos = Vector(0.0025, 6.5, -1.03),
         Ang = Angle(0, 0, 0),
         ViewModelFOV = 60,
         Magnification = 1,
         IgnoreExtra = true
     },
     {
-        Pos = Vector(0.675, 6.5, -1.725),
-        Ang = Angle(0, 0.75, 0),
-        ViewModelFOV = 60,
-        Magnification = 1.25,
-        IgnoreExtra = false,
-        Disassociate = true
-    },
-    {
-        Pos = Vector(0, 8, -2.425),
+        Pos = Vector(0, 7, -2.175),
         Ang = Angle(0, 0, 0),
         ViewModelFOV = 60,
         Magnification = 1,
@@ -63,7 +48,7 @@ ATT.RTScopeSubmatIndex = 0
 ATT.RTScopeFOV = 8
 ATT.RTScopeRes = 512
 ATT.RTScopeSurface = Material("models/weapons/arc9/mw3/mw3_optics/hamr_lens")
-ATT.RTScopeReticle = Material("hud/arc9_mwc/scopes/mwc_acog_realism.png", "mips smooth")
+ATT.RTScopeReticle = Material("hud/arc9_mwc/scopes/mwc_hamr.png", "mips smooth")
 ATT.RTScopeReticleScale = 0.85
 ATT.RTScopeShadowIntensity = 1.5
 ATT.RTScopeNoPP = false
