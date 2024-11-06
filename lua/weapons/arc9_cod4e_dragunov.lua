fba4e9f76b3d4275465d5ee0ab7aadd34785a70f
VinierAardvark1 <jackprovine@gmail.com>
2024-11-06 17:06:41 -0600

    just a little nudge
    pushed CoD4 Dragonuv's CustomizePos up by 1

--- lua/weapons/arc9_cod4e_dragunov.lua
+++ lua/weapons/arc9_cod4e_dragunov.lua
@@ -217,7 +217,7 @@ SWEP.SprintVerticalOffset = false
 SWEP.SprintPos = SWEP.ActivePos
 SWEP.SprintAng = SWEP.ActiveAng
 
-SWEP.CustomizePos = Vector(18.25, 27.5, 4)
+SWEP.CustomizePos = Vector(18.25, 27.5, 5)
 SWEP.CustomizeAng = Angle(90, 0, 0)
 SWEP.CustomizeSnapshotFOV = 100
 
